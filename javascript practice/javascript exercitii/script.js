a4901003c1ab6745b25dc033c2ff1c4be113ebbf
Robert Grapffel <robert.grapffel@softvision.ro>
2019-07-23 14:03:48 +0300

    final exercitiu 3

--- javascript practice/javascript exercitii/script.js
+++ javascript practice/javascript exercitii/script.js
@@ -1,28 +1,30 @@
 console.log("CONNECTED");
 
 var buttonClick = document.getElementById("buttonShow");
-var imagine = document.getElementById("imagine");
+var image = document.getElementById("imagine");
 var rmvImg = document.getElementById("removeBtn");
-var bodyClick = document.querySelector("body");
+/*var bodyClick = document.querySelector("body");*/
+var imageAgain = document.getElementById("showAgain");
 
 function displayImage(event) {
-  imagine.style.display = "block";
+  image.style.display = "block";
 }
 function hideImage(event) {
-  imagine.style.display = "none";
+  image.style.display = "none";
 }
 function removeDOM(event) {
-  imagine.parentNode.removeChild(imagine);
+  imagine.parentNode.removeChild(image);
   console.log("Imaginea a fost scoase");
   alert("Imaginea a fost stearsa");
 }
 
-function somethingClicked(event) {
+/*function somethingClicked(event) {
   console.log(event.target + " " + event.type);
   alert("Something was clicked!");
-}
+}*/
 
 buttonClick.addEventListener("click", displayImage);
 rmvImg.addEventListener("click", removeDOM);
-imagine.addEventListener("click", hideImage);
-bodyClick.addEventListener("click", somethingClicked);
+image.addEventListener("click", hideImage);
+/*bodyClick.addEventListener("click", somethingClicked);*/
+imageAgain.addEventListener("click", displayImage);

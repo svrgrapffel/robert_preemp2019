797a4ec57fe8ce8501385db29c0ec94b58e1787e
Robert Grapffel <robert.grapffel@softvision.ro>
2019-07-23 13:53:24 +0300

    final exercitiu 2

--- javascript practice/javascript exercitii/script.js
+++ javascript practice/javascript exercitii/script.js
@@ -1,9 +1,9 @@
 console.log("CONNECTED");
-/*Exercitui 1 */
 
 var buttonClick = document.getElementById("buttonShow");
 var imagine = document.getElementById("imagine");
 var rmvImg = document.getElementById("removeBtn");
+var bodyClick = document.querySelector("body");
 
 function displayImage(event) {
   imagine.style.display = "block";
@@ -14,9 +14,15 @@ function hideImage(event) {
 function removeDOM(event) {
   imagine.parentNode.removeChild(imagine);
   console.log("Imaginea a fost scoase");
-  alert("Imaginea nu exista");
+  alert("Imaginea a fost stearsa");
+}
+
+function somethingClicked(event) {
+  console.log(event.target + " " + event.type);
+  alert("Something was clicked!");
 }
 
 buttonClick.addEventListener("click", displayImage);
 rmvImg.addEventListener("click", removeDOM);
 imagine.addEventListener("click", hideImage);
+bodyClick.addEventListener("click", somethingClicked);

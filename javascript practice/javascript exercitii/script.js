4b9b4426a181cbebd3fc19576fda79763bc4fe46
Robert Grapffel <robert.grapffel@softvision.ro>
2019-07-23 11:59:09 +0300

    exercitiu unu

---
+++ javascript practice/javascript exercitii/script.js
@@ -0,0 +1,12 @@
+console.log("CONNECTED");
+/*Exercitui 1 : Sa creem un buton care sa arata o imagine atunci cand dam click pe el*/
+
+var buttonClick = document.getElementById("buttonClick");
+var imagine = document.getElementById("imagine");
+
+function displayImage(event) {
+  imagine.style.display = "block";
+}
+
+buttonClick.addEventListener("click", displayImage);
+hgdgdggdf;

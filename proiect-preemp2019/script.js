ff2c10dbb10787d214bf2bae9d090b127d47a4a3
Robert Grapffel <robert.grapffel@softvision.ro>
2019-07-24 15:35:22 +0300

    added functionality to modal

--- proiect-preemp2019/script.js
+++ proiect-preemp2019/script.js
@@ -1,6 +1,9 @@
 console.log("CONNECTED");
 var optionClick = document.querySelector(".options");
 var subOptions = document.getElementById("sublist");
+var products = document.querySelector(".btm-right-product");
+var modal = document.getElementById("modal");
+var closeModalPg = document.getElementById("closeModal");
 
 function showSuboptions() {
   if (subOptions.style.display === "none") {
@@ -10,3 +13,15 @@ function showSuboptions() {
   }
 }
 optionClick.addEventListener("click", showSuboptions);
+
+/*Se adauga functionalitate la modal*/
+
+function showModal() {
+  modal.style.display = "block";
+}
+
+function closeModals() {
+  modal.style.display = "none";
+}
+products.addEventListener("click", showModal);
+closeModalPg.addEventListener("click", closeModals);

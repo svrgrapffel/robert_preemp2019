f534bd75d04b88ab9039e5fa04f8147907b49708
Robert Grapffel <robert.grapffel@softvision.ro>
2019-08-05 10:22:07 +0300

    Cateva exercitii de nodeJs

--- NodeJsExercitii/nodeExercitiu.js
+++ NodeJsExercitii/nodeExercitiu.js
@@ -1,24 +1,48 @@
-const BasicEmitter = {
-  events: [],
-  on: function(type, listener) {
-    this.events[type] = this.events[type] || [];
-    this.events[type].push(listener);
-  },
-  emit: function(type) {
-    if (this.events[type]) {
-      this.events[type].forEach(function(listener) {
-        listener();
-      });
-    }
-  }
-};
-const emitter = Object.create(BasicEmitter);
-emitter.on("greet", function() {
-  console.log("Hello 2");
-});
+// const BasicEmitter = {
+//   events: [],
+//   on: function(type, listener) {
+//     this.events[type] = this.events[type] || [];
+//     this.events[type].push(listener);
+//   },
+//   emit: function(type) {
+//     if (this.events[type]) {
+//       this.events[type].forEach(function(listener) {
+//         listener();
+//       });
+//     }
+//   }
+// };
+// const emitter = Object.create(BasicEmitter);
+// emitter.on("greet", function() {
+//   console.log("Hello 2");
+// });
 
-emitter.on("greet", function() {
-  console.log("Hello 1");
+// emitter.on("greet", function() {
+//   console.log("Hello 1");
+// });
+// console.log("Hello");
+// console.log("greet");
+/*--------------------------*/
+// const fs = require("fs");
+// const { EventEmitter } = require("events");
+
+// const emitter = new EventEmitter();
+
+// emitter.on("phase", () => console.log("Poll phase")).emit("phase");
+// fs.readFile(__filename, () => {
+//   setTimeout(() => console.log("Timers phase"));
+//   setImmediate(() => console.log("Checck phase"));
+// });
+
+const http = require("http");
+const server = http.createServer();
+
+server
+  .on("close", () => {
+    console.log("called from Close callbacks phase");
+  })
+  .listen(8000);
+
+process.on("SIGINT", () => {
+  server.close();
 });
-console.log("Hello");
-console.log("greet");
